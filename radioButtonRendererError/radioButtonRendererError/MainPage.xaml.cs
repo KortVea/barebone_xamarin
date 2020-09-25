5b0637ec0c14f5435dffc75ecc1ed40f323b4f01
isaah <yishi.liu@smartcaptech.com>
2020-09-25 14:50:03 +1000

    Add project files.

---
+++ radioButtonRendererError/radioButtonRendererError/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace radioButtonRendererError
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool flag;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void Button_OnClicked(object sender, EventArgs e)
+        //{
+        //    Application.Current.UserAppTheme = flag ? OSAppTheme.Dark : OSAppTheme.Light;
+        //    flag = !flag;
+        //}
+    }
+}
